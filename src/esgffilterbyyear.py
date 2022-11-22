52edfbbb89eeb0046e279dbd7a7229a3444cca6c
Haimasree <haimasree.il@gmail.com>
2022-11-22 16:02:01 +0200

    basic command line working with pyproject.toml

--- src/esgffilterbyyear.py
+++ src/esgffilterbyyear.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Optional
 
 import click
@@ -12,15 +13,17 @@ import click
     type=click.Path(exists=True, readable=True),
     required=False,
 )
-@click.argument("start_year", required=True, type=click.int)
-@click.argument("end_year", required=True, type=click.int)
+@click.argument("start_year", required=True, type=click.INT)
+@click.argument("end_year", required=True, type=click.INT)
 def filter_by_year(
     input_file: click.Path,
     output_file: Optional[click.Path],
-    start_year: click.int,
-    end_year: click.int,
+    start_year: click.INT,
+    end_year: click.INT,
 ):
-    print("Hello world")
+    if output_file is None:
+        output_file = f"filtered-{Path(input_file).stem}{Path(input_file).suffixes[-1]}"
+    print(f"Hello world with {input_file}, {output_file}, {start_year} and {end_year}")
 
 
 if __name__ == "__main__":
