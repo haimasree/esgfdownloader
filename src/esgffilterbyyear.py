85baa9628cdd17dbde1bb67ea523022e4ea66e03
Haimasree <haimasree.il@gmail.com>
2022-11-22 16:18:09 +0200

    basic function added

--- src/esgffilterbyyear.py
+++ src/esgffilterbyyear.py
@@ -4,13 +4,19 @@ from typing import Optional
 import click
 
 
+def filter_and_write(
+    input_file: Path, output_file: Path, start_year: int, end_year: int
+):
+    print("Entered a function")
+
+
 @click.command()
 @click.argument("input_file", type=click.Path(exists=True, readable=True))
 @click.option(
     "-o",
     "--output",
     "output_file",
-    type=click.Path(exists=True, readable=True),
+    type=click.Path(),
     required=False,
 )
 @click.argument("start_year", required=True, type=click.INT)
@@ -21,8 +27,15 @@ def filter_by_year(
     start_year: click.INT,
     end_year: click.INT,
 ):
+    input_file = Path(input_file)
     if output_file is None:
-        output_file = f"filtered-{Path(input_file).stem}{Path(input_file).suffixes[-1]}"
+        output_file = f"filtered-{input_file.stem}{input_file.suffixes[-1]}"
+    filter_and_write(
+        input_file=input_file,
+        output_file=output_file,
+        start_year=start_year,
+        end_year=end_year,
+    )
     print(f"Hello world with {input_file}, {output_file}, {start_year} and {end_year}")
 
 
