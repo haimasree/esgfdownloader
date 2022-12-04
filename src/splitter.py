c9b26dedd4d77edf8da21ccd9b6555d8be0e3949
Haimasree <haimasree.il@gmail.com>
2022-12-04 14:19:37 +0200

    adding splitting by directory

--- src/splitter.py
+++ src/splitter.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import List, Optional
 from pathlib import Path
 
 import numpy as np
@@ -11,11 +11,14 @@ STR_PATTERN = "SHA256"
 
 # TODO: Add a chunk version for files which can not be loaded into memory
 
+
 def split_and_write(
     input_file: Path, output_files: List[Path], number_of_splits: int
 ) -> None:
     with open(input_file, "r") as input_file_handle:
-        content = input_file_handle.read() # Assuming the file is large enough to be loaded into memory
+        content = (
+            input_file_handle.read()
+        )  # Assuming the file is large enough to be loaded into memory
     start_template = content.split(START_PATTERN)[0]
     urls = content.split(END_PATTERN)[
         1
@@ -39,19 +42,41 @@ def split_and_write(
 
 
 @click.command()
-@click.argument("input_file", type=click.Path(exists=True, readable=True))
+@click.argument("input_filedir", type=click.Path(exists=True, readable=True))
+@click.option(
+    "-o",
+    "--output",
+    "output_filedir",
+    type=click.Path(),
+    required=False,
+)
 @click.argument("number_of_splits", required=True, type=click.INT)
-def split_cli(input_file: click.Path, number_of_splits: click.INT):
-    input_file = Path(input_file)
-    output_files = [
-        Path(f"split-{input_file.stem}-{n}{input_file.suffixes[-1]}")
-        for n in range(number_of_splits)
-    ]
-    split_and_write(
-        input_file=input_file,
-        output_files=output_files,
-        number_of_splits=number_of_splits,
-    )
+def split_cli(
+    input_filedir: click.Path,
+    output_filedir: Optional[click.Path],
+    number_of_splits: click.INT,
+):
+    input_filedir = Path(input_filedir)
+    if output_filedir is None:
+        output_filedir = input_filedir.parent / "split"
+    else:
+        output_filedir = Path(output_filedir)
+    output_filedir.mkdir(exist_ok=True)
+    for input_file in input_filedir.rglob("wget-*.sh"):
+        output_files = [
+            Path(
+                output_filedir / f"split-{input_file.stem}-{n}{input_file.suffixes[-1]}"
+            )
+            for n in range(number_of_splits)
+        ]
+        print(
+            f"Splitting {input_file} into {number_of_splits} files and writing to {output_filedir}"
+        )
+        split_and_write(
+            input_file=input_file,
+            output_files=output_files,
+            number_of_splits=number_of_splits,
+        )
 
 
 if __name__ == "__main__":
