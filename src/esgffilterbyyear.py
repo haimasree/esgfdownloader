7ab201e8cf074cecc8bf7971a1a23ad121e413e1
Haimasree <haimasree.il@gmail.com>
2022-12-04 13:49:42 +0200

    adding filter tool to operate across a directory

--- src/esgffilterbyyear.py
+++ src/esgffilterbyyear.py
@@ -39,47 +39,52 @@ def filter_and_write(
             stripped_line = line.rstrip()
             if stripped_line == START_PATTERN:
                 match = True
-                print("Starting the filtering process")
             elif stripped_line == END_PATTERN:
                 match = False
-                print("Ending the filtering process")
             if match:
                 output = filter_by_year(stripped_line, line, desired_interval)
                 if output != "NOLINE":
                     output_file_handle.write(line)
-                else:
-                    print(f"{stripped_line} is not part of the given years")
+                # else:
+                #     print(f"{stripped_line} is not part of the given years")
             else:
                 output_file_handle.write(line)
 
 
 @click.command()
-@click.argument("input_file", type=click.Path(exists=True, readable=True))
+@click.argument("input_filedir", type=click.Path(exists=True, readable=True))
 @click.option(
     "-o",
     "--output",
-    "output_file",
+    "output_filedir",
     type=click.Path(),
     required=False,
 )
 @click.argument("start_year", required=True, type=click.INT)
 @click.argument("end_year", required=True, type=click.INT)
 def filter_cli(
-    input_file: click.Path,
-    output_file: Optional[click.Path],
+    input_filedir: click.Path,
+    output_filedir: Optional[click.Path],
     start_year: click.INT,
     end_year: click.INT,
 ):
-    input_file = Path(input_file)
-    if output_file is None:
-        output_file = Path(f"filtered-{input_file.stem}{input_file.suffixes[-1]}")
-    filter_and_write(
-        input_file=input_file,
-        output_file=output_file,
-        start_year=start_year,
-        end_year=end_year,
-    )
-    print(f"Filtering {input_file}, {output_file}, {start_year} and {end_year}")
+    input_filedir = Path(input_filedir)
+    if output_filedir is None:
+        output_filedir = input_filedir.parent / "filtered"
+    else:
+        output_filedir = Path(output_filedir)
+    output_filedir.mkdir(exist_ok=True)
+    for input_file in input_filedir.rglob("wget-*.sh"):
+        output_file = Path(
+            output_filedir / f"filtered-{input_file.stem}{input_file.suffixes[-1]}"
+        )
+        print(f"Running filter {input_file} {output_file} {start_year} {end_year}")
+        filter_and_write(
+            input_file=input_file,
+            output_file=output_file,
+            start_year=start_year,
+            end_year=end_year,
+        )
 
 
 if __name__ == "__main__":
