a655ebfde6670c3fa63324e09d3afc5dde6ea5e5
Haimasree <haimasree.il@gmail.com>
2023-02-13 18:39:17 +0200

    adding full logic

--- src/qc.py
+++ src/qc.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pathlib import Path
+import shutil
 
 import click
 
@@ -34,22 +35,32 @@ def qc_cli(
     else:
         outputdir = Path(outputdir)
     outputdir.mkdir(exist_ok=True)
+
+    allfilenames = [
+        [input_file.name for input_file in inputdir.rglob("*.*")]
+        for inputdir in inputdirlist
+    ]
+    common_filenames = set.intersection(*[set(filenames) for filenames in allfilenames])
+
     uniquefilelist = {}
     for inputdir in inputdirlist:
         for input_file in inputdir.rglob("*.*"):
-            if input_file.name not in uniquefilelist:
-                uniquefilelist[input_file.name] = {
-                    "path": input_file.resolve(),
-                    "size": input_file.stat().st_size,
-                }
-            elif uniquefilelist[input_file.name]["size"] < input_file.stat().st_size:
-                uniquefilelist[input_file.name] = {
-                    "path": input_file.resolve(),
-                    "size": input_file.stat().st_size,
-                }
+            if input_file.name in common_filenames:
+                if input_file.name not in uniquefilelist:
+                    uniquefilelist[input_file.name] = {
+                        "path": input_file.resolve(),
+                        "size": input_file.stat().st_size,
+                    }
+                elif (
+                    uniquefilelist[input_file.name]["size"] < input_file.stat().st_size
+                ):
+                    uniquefilelist[input_file.name] = {
+                        "path": input_file.resolve(),
+                        "size": input_file.stat().st_size,
+                    }
 
-    for item, val in uniquefilelist.items():
-        print(f"path = {val['path']}, size={val['size']}")
+    for filename, fileinfo in uniquefilelist.items():
+        shutil.copyfile(fileinfo["path"], outputdir / filename)
 
 
 if __name__ == "__main__":
